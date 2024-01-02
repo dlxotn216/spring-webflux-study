924d5d8f89583a56b58d21a35a676d85192b335b
taesu <taesu@crscube.co.kr>
2024-01-03 08:25:44 +0900

    Project initial commit

---
+++ src/main/kotlin/me/taesu/reactorwebfluxstudy/ReactorWebfluxStudyApplication.kt
@@ -0,0 +1,11 @@
+package me.taesu.reactorwebfluxstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactorWebfluxStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReactorWebfluxStudyApplication>(*args)
+}
---
+++ src/test/kotlin/me/taesu/reactorwebfluxstudy/ReactorWebfluxStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.taesu.reactorwebfluxstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactorWebfluxStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
